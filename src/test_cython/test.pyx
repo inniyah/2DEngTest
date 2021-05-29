c7448ff84e4de96f2c7ccd0f5ef62f62736681d1
Miriam Ruiz <miriam@debian.org>
2021-05-29 23:08:30 +0200

    Implemented SDL2-gpu example

--- src/test_cython/test.pyx
+++ src/test_cython/test.pyx
@@ -12,15 +12,32 @@ from cpython.ref cimport PyObject
 from cython.operator cimport dereference as deref
 from enum import IntEnum
 
+# Thread-safe solution using c++11
+# See: https://stackoverflow.com/questions/40976880/canonical-way-to-generate-random-numbers-in-cython
+cdef extern from "<random>" namespace "std":
+    cdef cppclass mt19937:
+        mt19937() # we need to define this constructor to stack allocate classes in Cython
+        mt19937(unsigned int seed) # not worrying about matching the exact int type for seed
+    cdef cppclass uniform_real_distribution[T]:
+        uniform_real_distribution()
+        uniform_real_distribution(T a, T b)
+        T operator()(mt19937 gen) # ignore the possibility of using other classes for "gen"
+    cdef cppclass discrete_distribution[T]:
+        discrete_distribution()
+        # The following constructor is really a more generic template class
+        # but tell Cython it only accepts vector iterators
+        discrete_distribution(vector.iterator first, vector.iterator last)
+        T operator()(mt19937 gen)
+
 import sys
 
 print("Hello World")
 
 cdef class _SDL2TestApplication:
-    cdef SDL2.SDL_Window* win
-    cdef SDL2.SDL_Renderer* ren
-    cdef SDL2.SDL_Surface* bmp
-    cdef SDL2.SDL_Texture* tex
+    cdef SDL2.SDL_Window * win
+    cdef SDL2.SDL_Renderer * ren
+    cdef SDL2.SDL_Surface * bmp
+    cdef SDL2.SDL_Texture * tex
 
     def __cinit__(self):
         if (SDL2.SDL_Init(SDL2.SDL_INIT_EVERYTHING) != 0):
@@ -395,8 +412,10 @@ class TiledTestApplication(_TiledTestApplication):
     pass
 
 cdef class _SdlGpuTest:
+    cdef SDL2_gpu.GPU_Target * _screen
+
     def __cinit__(self):
-        pass
+        self._screen = NULL
 
     def printRenderers(self):
         cdef SDL2_gpu.SDL_version compiled = SDL2_gpu.GPU_GetCompiledVersion()
@@ -430,7 +449,7 @@ cdef class _SdlGpuTest:
 
     def printCurrentRenderer(self):
         cdef SDL2_gpu.GPU_Renderer* renderer = SDL2_gpu.GPU_GetCurrentRenderer()
-        cdef SDL2_gpu.GPU_RendererID id = renderer.id;
+        cdef SDL2_gpu.GPU_RendererID id = renderer.id
         SDL2_gpu.GPU_LogInfo("Using renderer: %s (%d.%d)\n",
             id.name, id.major_version, id.minor_version)
         SDL2_gpu.GPU_LogInfo(" Shader versions supported: %d to %d\n\n",
@@ -438,10 +457,91 @@ cdef class _SdlGpuTest:
 
     def init(self):
         SDL2_gpu.GPU_SetPreInitFlags(SDL2_gpu.GPU_INIT_DISABLE_VSYNC)
-        screen = SDL2_gpu.GPU_Init(800, 600, SDL2_gpu.GPU_DEFAULT_INIT_FLAGS)
-        if screen == NULL:
+        self._screen = SDL2_gpu.GPU_Init(800, 600, SDL2_gpu.GPU_DEFAULT_INIT_FLAGS)
+        if self._screen == NULL:
             SDL2_gpu.GPU_LogError("GPU_Init Failed!")
             return -1
+
+    def run(self):
+        cdef int maxSprites = 50000
+        cdef int numSprites = 101
+        cdef float dt = 0.010
+
+        cdef float * x = <float*>malloc(sizeof(float) * maxSprites)
+        cdef float * y = <float*>malloc(sizeof(float) * maxSprites)
+        cdef float * velx = <float*>malloc(sizeof(float) * maxSprites)
+        cdef float * vely = <float*>malloc(sizeof(float) * maxSprites)
+
+        cdef SDL2_gpu.GPU_Image * image = SDL2_gpu.GPU_LoadImage("img/small_test.png")
+        if image == NULL:
+            SDL2_gpu.GPU_LogError("GPU_LoadImage Failed!")
+            return -1
+
+        SDL2_gpu.GPU_SetSnapMode(image, SDL2_gpu.GPU_SNAP_NONE)
+
+        cdef mt19937 gen = mt19937(5)
+        cdef uniform_real_distribution[double] dist = uniform_real_distribution[double](0.0, 1.0)
+
+        cdef SDL2.Uint32 startTime = SDL2.SDL_GetTicks()
+        cdef long frameCount = 0
+        for i in range(maxSprites):
+            x[i] = dist(gen) * self._screen.w
+            y[i] = dist(gen) * self._screen.h
+            velx[i] = dist(gen) * self._screen.w / 10.0 - self._screen.w / 20.0
+            vely[i] = dist(gen) * self._screen.h / 10.0 - self._screen.h / 20.0
+
+        done = False
+        cdef SDL2.SDL_Event event
+        while not done:
+            while SDL2.SDL_PollEvent(&event):
+                if event.type == SDL2.SDL_QUIT:
+                    done = True
+                elif event.type == SDL2.SDL_KEYDOWN:
+                    if event.key.keysym.sym == SDL2.SDLK_ESCAPE:
+                        done = True
+                    elif event.key.keysym.sym == SDL2.SDLK_EQUALS or event.key.keysym.sym == SDL2.SDLK_PLUS:
+                        if numSprites < maxSprites:
+                            numSprites += 100
+                        SDL2_gpu.GPU_LogError("Sprites: %d\n", numSprites)
+                        frameCount = 0
+                        startTime = SDL2.SDL_GetTicks()
+                    elif event.key.keysym.sym == SDL2.SDLK_MINUS:
+                        if numSprites > 1:
+                            numSprites -= 100
+                        if numSprites < 1:
+                            numSprites = 1
+                        SDL2_gpu.GPU_LogError("Sprites: %d\n", numSprites)
+                        frameCount = 0
+                        startTime = SDL2.SDL_GetTicks()
+
+            for i in range(numSprites):
+                x[i] += velx[i] * dt
+                y[i] += vely[i] * dt
+                if x[i] < 0:
+                    x[i] = 0
+                    velx[i] = -velx[i]
+                elif x[i] > self._screen.w:
+                    x[i] = self._screen.w
+                    velx[i] = -velx[i]
+                if y[i] < 0:
+                    y[i] = 0
+                    vely[i] = -vely[i]
+                elif y[i]> self._screen.h:
+                    y[i] = self._screen.h
+                    vely[i] = -vely[i]
+
+            SDL2_gpu.GPU_Clear(self._screen)
+
+            for i in range(numSprites):
+                SDL2_gpu.GPU_Blit(image, NULL, self._screen, x[i], y[i])
+
+            SDL2_gpu.GPU_Flip(self._screen)
+
+            frameCount += 1
+            if SDL2.SDL_GetTicks() - startTime > 5000:
+                SDL2_gpu.GPU_LogError("Average FPS: %.2f\n", 1000.0 * frameCount / (SDL2.SDL_GetTicks() - startTime))
+                frameCount = 0
+                startTime = SDL2.SDL_GetTicks()
 
     def quit(self):
         SDL2_gpu.GPU_Quit()
