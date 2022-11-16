7bf3bf8e0950252c5e263c04e892adc43c1f147c
Miriam Ruiz <miriam@debian.org>
2022-11-16 21:43:23 +0100

    Improved logging

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
+#~ import faulthandler
 import logging
 import os
 import sys
+
+from datetime import datetime
+from pprint import pprint
 
 MY_PATH = os.path.normpath(os.path.abspath(os.path.dirname(__file__)))
 sys.path.append(os.path.abspath(os.path.join(MY_PATH, 'python')))
@@ -131,6 +135,27 @@ def runRaylib():
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+# Dump logging output with colors
+
+class ColorStderr(logging.StreamHandler):
+    def __init__(self, fmt=None):
+        class AddColor(logging.Formatter):
+            def __init__(self):
+                super().__init__(fmt)
+            def format(self, record: logging.LogRecord):
+                msg = super().format(record)
+                # Green/Cyan/Yellow/Red/Redder based on log level:
+                color = '\033[1;' + ('32m', '36m', '33m', '31m', '41m')[min(4,int(4 * record.levelno / logging.FATAL))]
+                return color + record.levelname + '\033[1;0m: ' + msg
+        super().__init__(sys.stderr)
+        self.setFormatter(AddColor())
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+LOG_FILE_FORMAT = "[%(levelname)s] [%(pathname)s:%(lineno)d] [%(asctime)s] [%(name)s]: '%(message)s'"
+LOG_CONSOLE_FORMAT = "[%(pathname)s:%(lineno)d] [%(asctime)s]: '%(message)s'"
+LOG_GUI_FORMAT = "[%(levelname)s] %(message)s"
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-q", "--quiet", help="set logging to ERROR",
@@ -140,7 +165,28 @@ def main():
                         action="store_const", dest="loglevel",
                         const=logging.DEBUG, default=logging.INFO)
     parser.add_argument("-t", "--test", dest="test", required=False, help="Test to run", default=None)
+    parser.add_argument('--log', action=argparse.BooleanOptionalAction)
     args = parser.parse_args()
+
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.handlers = []
+
+    log_console_handler = ColorStderr(LOG_CONSOLE_FORMAT)
+    log_console_handler.setLevel(args.loglevel)
+    logger.addHandler(log_console_handler)
+
+    if args.log:
+        now = datetime.now()
+        logs_dir = os.path.abspath(os.path.join(MY_PATH, "logs", f"{now.strftime('%Y%m%d')}"))
+        os.makedirs(logs_dir, exist_ok=True)
+        log_filename = f"{now.strftime('%Y%m%d')}_{now.strftime('%H%M%S')}.txt"
+        log_file_handler = logging.FileHandler(os.path.join(logs_dir, log_filename))
+        log_formatter = logging.Formatter(LOG_FILE_FORMAT)
+        log_file_handler.setFormatter(log_formatter)
+        log_file_handler.setLevel(logging.DEBUG)
+        logger.addHandler(log_file_handler)
+        logging.info(f"Storing log into '{logs_dir}/{log_filename}'")
 
     if args.test is None:
         tests = [ 'light' ]
@@ -178,4 +224,5 @@ def main():
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 if __name__ == "__main__":
+    #~ faulthandler.enable()
     main()
